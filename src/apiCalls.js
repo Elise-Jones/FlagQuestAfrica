c540f7e60fc8d3d142c817f10ab95b022c4def66
Elise Jones <02elise.jones02@gmail.com>
2023-07-31 18:50:46 -0600

    Create fetch cal

---
+++ src/apiCalls.js
@@ -0,0 +1,12 @@
+const fetchAfricanCountries = () => {
+  return fetch('https://restcountries.com/v3.1/region/Africa')
+  .then((resp) => {
+    if(resp.ok){
+      return resp.json()
+    } else {
+      throw new Error("Sorry something went wrong")
+    }
+  }).then(data => console.log(data))
+  
+}
+fetchAfricanCountries()
