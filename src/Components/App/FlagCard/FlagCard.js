856666bfcbe819d2b8d7ea0dff350024f12712b8
Elise Jones <02elise.jones02@gmail.com>
2023-08-03 09:33:47 -0600

    Remove uneeded props

--- src/Components/App/FlagCard/FlagCard.js
+++ src/Components/App/FlagCard/FlagCard.js
@@ -3,11 +3,11 @@ import './FlagCard.css';
 import { Form } from '../../Form/Form';
 import { useLocation } from 'react-router-dom';
 
-export const FlagCard = ({ singleFlag, setSingleFlag, alert, setAlert, deleteFlag }) => {
+export const FlagCard = ({ singleFlag, setSingleFlag, deleteFlag }) => {
   const { id, flagPng, altText, name } = singleFlag;
-  
-  console.log(alert)
+  const [alert, setAlert] = React.useState('');
   const location = useLocation()
+  
   return (
     <div className="flag-card">
       <img src={flagPng} alt={altText}  />
