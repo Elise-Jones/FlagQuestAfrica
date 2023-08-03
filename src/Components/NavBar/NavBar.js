f6d8e6cfbce5d763c1593745671f765b4ac51a16
Elise Jones <02elise.jones02@gmail.com>
2023-08-03 17:08:35 -0600

    Add flex box to elements

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import "./NavBar.css"
 import {  NavLink } from 'react-router-dom';
 
 export const NavBar = () => {
   return (
-    <nav>
-      <NavLink to="/">home</NavLink>
-      <NavLink to="saved">Saved for Later</NavLink>
+    <nav className="nav-container">
+      <NavLink to="/" className="homenav">home</NavLink>
+      <NavLink to="saved" className="savenav">Saved for Later</NavLink>
     </nav>
   )
 }
