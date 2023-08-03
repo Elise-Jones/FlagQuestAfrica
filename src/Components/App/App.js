52253c64fe4e1a7e9c59242198a8a30c9cf006a3
Elise Jones <02elise.jones02@gmail.com>
2023-08-02 20:48:52 -0600

    Remove uneeded code

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import {fetchAfricanCountries} from "../../apiCalls"
-// import { countryData, oneCountry } from '../../mockData';
 import { cleanData, getRandomNumber } from '../../utils';
 import { useEffect, useState } from 'react';
 import { Homepage } from '../Homepage/Homepage';
@@ -13,17 +12,12 @@ function App() {
   const [singleFlag, setSingleFlag] = useState([]);
   const [savedFlags, setSavedFlag] = useState([])
   const [alert, setAlert] = useState("");
-  
-
-
 
   const addFlag = (newFlag) => {
-  setSavedFlag((prev)=> [...prev, newFlag])
-    
+    setSavedFlag((prev)=> [...prev, newFlag])
   }
 
   const deleteFlag =(name) => {
-    
    const filteredFlags = savedFlags.filter(flag => flag.name!== name) 
    setSavedFlag(filteredFlags)
   }
@@ -39,11 +33,6 @@ function App() {
   useEffect(() => {
     getFlag()
   }, [])
-
-  useEffect(() => {
-    console.log("alert in app", alert)
-  }, [])
-
 
   return (
     <div className="App">
--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,17 +1,14 @@
-import React, { useEffect } from "react";
 import { FlagCard } from "../App/FlagCard/FlagCard";
-import { useState } from "react";
 import './Homepage.css';
 export const Homepage = ({ singleFlag, setSingleFlag, addFlag, getFlag, setAlert, alert  }) => {
   
-
-
   const handleClick =() => {
     const newFlag = singleFlag
     addFlag(newFlag)
     setAlert("")
     getFlag()
   }
+
   return (
     <div className="container">
       {/* img here */}
@@ -20,7 +17,6 @@ export const Homepage = ({ singleFlag, setSingleFlag, addFlag, getFlag, setAlert
         setSingleFlag={setSingleFlag}
         alert={alert}
         setAlert={setAlert}
-        
       />
       <button onClick={getFlag}>Show me new </button>
       <button onClick={handleClick}>Save for later </button>
