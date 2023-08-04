b2ef60ad8393f22b9983956ac16c1feaa1dc9a0e
Elise Jones <02elise.jones02@gmail.com>
2023-08-04 14:22:51 -0600

    Add .then after waits

--- cypress/e2e/homepage_spec.cy.js
+++ cypress/e2e/homepage_spec.cy.js
@@ -7,6 +7,7 @@ describe("template spec", () => {
       .as("fetchCountry1")
       .visit("http://localhost:3000/");
   });
+
   it("should visit home page and see nav bar with a flag and a form", () => {
     cy.wait("@fetchCountry1")
       .its("response.statusCode")
@@ -42,45 +43,46 @@ describe("template spec", () => {
   });
 
   it("should be able to fill out form to test your flag knowledge and get more flags", () => {
-    cy.wait("@fetchCountry1")
-      .then((interception) => {
-        cy.get("form")
-          .find('[placeholder="What country?"]')
-          .type("Morocco")
-          .should("have.value", "Morocco")
-          .get(".submit-button")
-          .click()
-          .get("form > p")
-          .should("be.visible")
-          .contains("You are incorrect, try again.")
+    cy.wait("@fetchCountry1").then((interception) => {
+      cy.get("form")
+        .find('[placeholder="What country?"]')
+        .type("Morocco")
+        .should("have.value", "Morocco")
+        .get(".submit-button")
+        .click()
+        .get("form > p")
+        .should("be.visible")
+        .contains("You are incorrect, try again.")
+        .get("form")
+        .find('[placeholder="What country?"]')
+        .type("Republic of Guinea")
+        .should("have.value", "Republic of Guinea")
+        .get(".submit-button")
+        .click()
+        .get("form > p")
+        .should("be.visible")
+        .contains("You are correct!");
+
+      cy.intercept("GET", "https://restcountries.com/v3.1/region/Africa", {
+        statusCode: 200,
+        fixture: "country2",
+      }).as("fetchCountry2");
+
+      cy.get(".show-new-button").click().wait("@fetchCountry2").then((interception) => {
+        cy.get("img")
+          .should("have.attr", "src")
+          .should("include", "https://flagcdn.com/w320/ga.png")
           .get("form")
           .find('[placeholder="What country?"]')
-          .type("Republic of Guinea")
-          .should("have.value", "Republic of Guinea")
+          .type("Gabonese Republic")
+          .should("have.value", "Gabonese Republic")
           .get(".submit-button")
           .click()
           .get("form > p")
           .should("be.visible")
           .contains("You are correct!");
-        cy.intercept("GET", "https://restcountries.com/v3.1/region/Africa", {
-          statusCode: 200,
-          fixture: "country2",
-        }).as("fetchCountry2");
-      })
-    .get(".show-new-button")
-    .click()
-    .get("img")
-    .should("have.attr", "src")
-    .should("include", "https://flagcdn.com/w320/ga.png")
-    .get("form")
-    .find('[placeholder="What country?"]')
-    .type("Gabonese Republic")
-    .should("have.value", "Gabonese Republic")
-    .get(".submit-button")
-    .click()
-    .get("form > p")
-    .should("be.visible")
-    .contains("You are correct!");
+      });
+    });
   });
 
   it("Should have multiple routes from home to saved page", () => {
@@ -95,6 +97,5 @@ describe("template spec", () => {
       .find(".homenav")
       .click();
     cy.url().should("include", "/");
-    // neee to test that it wills how answer
   });
 });
