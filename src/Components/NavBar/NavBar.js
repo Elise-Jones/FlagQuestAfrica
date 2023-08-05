272818dc75a4f0567a458782e5770857a101921f
Elise Jones <02elise.jones02@gmail.com>
2023-08-05 15:42:52 -0600

    Change font size for buttons

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,8 +5,8 @@ import {  NavLink } from 'react-router-dom';
 export const NavBar = () => {
   return (
     <nav className="nav-container">
-      <NavLink to="/" className="homenav">home</NavLink>
-      <NavLink to="saved" className="savenav">Saved for Later</NavLink>
+      <NavLink to="/" className="homenav">HOME</NavLink>
+      <NavLink to="saved" className="savenav">YOUR SAVED FLAGS</NavLink>
     </nav>
   )
 }
