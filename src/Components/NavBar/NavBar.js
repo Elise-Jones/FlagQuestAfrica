3a7b33ae839ea081d75a0ba48c9a106539f2b4e1
Elise Jones <02elise.jones02@gmail.com>
2023-09-18 15:38:56 -0600

    Clean up css

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,6 +5,7 @@ import {  NavLink } from 'react-router-dom';
 export const NavBar = () => {
   return (
     <nav className="nav-container">
+      <h1> FlagQuest</h1>
       <NavLink to="/" className="homenav">HOME</NavLink>
       <NavLink to="saved" className="savenav">YOUR SAVED FLAGS</NavLink>
     </nav>
